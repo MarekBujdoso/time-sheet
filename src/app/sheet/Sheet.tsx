b5031daa07818dd12c1e734d5828cf9b58dc46e0
MarekBujdoso <marek.bujdoso@gmail.com>
2025-01-05 20:23:02 +0100

    add some temp data

--- src/app/sheet/Sheet.tsx
+++ src/app/sheet/Sheet.tsx
@@ -3,8 +3,20 @@ import MonthPager from "../../components/month-pager"
 
 const Sheet = () => {
   return (
-    <div className="flex w-full min-h-svh justify-top">
+    <div className="flex flex-col w-full min-h-svh justify-top border-2 border-black p-4 rounded-lg">
       <MonthPager />
+      <div className="flex flex-row w-full gap-4">
+        <div className="flex flex-col">
+          <span>odpracovane: 60h / 8d</span>
+          <span>P-cko: 4h / 0,5d</span>
+          <span>PN: 0h / 0d</span>
+        </div>
+        <div className="flex flex-col">
+          <span>nadcasy: 60h / 8d</span>
+          <span>P-cko dop.: 0h / 0d</span>
+          <span>OCR: 0h / 0d</span>
+        </div>
+      </div>
     </div>
   )
 }

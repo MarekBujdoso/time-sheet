4ff1490f9e131a8227e0ea79b9af3f045d503e8b
MarekBujdoso <marek.bujdoso@gmail.com>
2025-01-27 19:51:52 +0100

    change Month selector and show two days

--- src/app/sheet/Sheet.tsx
+++ src/app/sheet/Sheet.tsx
@@ -1,10 +1,15 @@
+import React from "react"
 import MonthPager from "../../components/month-pager"
+import { Button } from "../../components/ui/button"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../../components/ui/collapsible"
 
 
 const Sheet = () => {
+  const [open, setOpen] = React.useState(false)
+  const currentMonth = new Date().getMonth() + 1
   return (
     <div className="flex flex-col w-full min-h-svh justify-top border-2 border-black p-4 rounded-lg">
-      <MonthPager />
+      <MonthPager month={currentMonth}/>
       <div className="flex flex-row w-full gap-4">
         <div className="flex flex-col">
           <span>odpracovane: 60h / 8d</span>
@@ -16,6 +21,84 @@ const Sheet = () => {
           <span>P-cko dop.: 0h / 0d</span>
           <span>OCR: 0h / 0d</span>
         </div>
+      </div>
+      <div>
+        <Collapsible className="rounded-md border" open={open} onOpenChange={setOpen}>
+          <div className="flex flex-row gap-5 items-center p-2">
+            <span className="text-sm font-semibold">1.</span>
+            <div className="flex flex-col">
+              <span className="text-sm">7:30 - 15:30</span>
+              <span className="text-sm">obed: 0,5h</span>
+            </div>
+            <div className="flex flex-col grow">
+              <span className="text-sm">NV: 0h</span>
+              <span className="text-sm">P-cko: 0h</span>
+            </div>
+            <CollapsibleTrigger asChild>
+              <Button variant="outline" size="default">
+                {/* <ChevronsUpDown className="h-4 w-4" /> */}
+                <span className="text-lg font-semibold">7,5</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+
+          <CollapsibleContent className="space-y-2">
+            <div className="rounded-md border p-2 text-sm shadow-sm">
+              <div className="flex flex-row gap-5 items-center p-2">
+                <div className="flex flex-col">
+                  <span className="text-sm">7:30 - 15:30</span>
+                  <span className="text-sm">obed: 0,5h</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-sm">NV: 0h</span>
+                  <span className="text-sm">P-cko: 0h</span>
+                </div>
+                <div className="flex flex-col grow">
+                  <span className="text-sm">Praca doma: 0h</span>
+                  <span className="text-sm">OCR: 0h</span>
+                </div>
+              </div>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
+        <Collapsible className="rounded-md border" open={false}>
+          <div className="flex flex-row gap-5 items-center p-2">
+            <span className="text-sm font-semibold">2.</span>
+            <div className="flex flex-col">
+              <span className="text-sm">7:30 - 15:30</span>
+              <span className="text-sm">obed: 0,5h</span>
+            </div>
+            <div className="flex flex-col grow">
+              <span className="text-sm">NV: 0h</span>
+              <span className="text-sm">P-cko: 0h</span>
+            </div>
+            <CollapsibleTrigger asChild>
+              <Button variant="outline" size="default">
+                {/* <ChevronsUpDown className="h-4 w-4" /> */}
+                <span className="text-lg font-semibold">7,5</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+
+          <CollapsibleContent className="space-y-2">
+            <div className="rounded-md border p-2 text-sm shadow-sm">
+              <div className="flex flex-row gap-5 items-center p-2">
+                <div className="flex flex-col">
+                  <span className="text-sm">7:30 - 15:30</span>
+                  <span className="text-sm">obed: 0,5h</span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-sm">NV: 0h</span>
+                  <span className="text-sm">P-cko: 0h</span>
+                </div>
+                <div className="flex flex-col grow">
+                  <span className="text-sm">Praca doma: 0h</span>
+                  <span className="text-sm">OCR: 0h</span>
+                </div>
+              </div>
+            </div>
+          </CollapsibleContent>
+        </Collapsible>
       </div>
     </div>
   )
--- src/components/month-pager.tsx
+++ src/components/month-pager.tsx
@@ -1,20 +1,54 @@
+import React from "react"
 import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "./ui/pagination"
 
 
-const MonthPager = () => {
+const monthsMap: { [key: number]: string } = {
+  1: "Januar", 
+  2: "Februar", 
+  3: "Marec", 
+  4: "April", 
+  5: "Maj", 
+  6: "Jun", 
+  7: "Jul", 
+  8: "August", 
+  9: "September", 
+  10: "Oktober", 
+  11: "November", 
+  12: "December",
+}
+
+interface MonthPagerProps {
+  month: number
+}
+
+const MonthPager = ({ month }: MonthPagerProps) => {
+  const [activeMonth, setActiveMonth] = React.useState(month)
+  
+  const switchToPrevMonth = React.useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    setActiveMonth((prev) => prev > 1 ? prev - 1 : 12)
+  }, [])
+
+  const switchToNextMonth = React.useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    setActiveMonth((prev) => prev < 12 ? prev + 1 : 1)
+  }, [])
+
   return (
 //   <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
   <div className="flex  w-full">
     <div className="w-full max-w-sm">
       <Pagination >
-        <PaginationContent>
+        <PaginationContent className="w-full justify-between">
           <PaginationItem>
-            <PaginationPrevious href="#" />
+            <PaginationPrevious size="lg" className="select-none" onClick={switchToPrevMonth} />
           </PaginationItem>
-          <PaginationItem>
-            <PaginationLink size="default" href="#" isActive>September</PaginationLink>
-          </PaginationItem>
-          
+          {/* <PaginationItem>
+            <PaginationLink className="w-40 select-none" onClick={handleMonth} isActive>{monthsMap[activeMonth]}</PaginationLink>
+          </PaginationItem> */}
+          <div className="w-40 select-none">
+            {monthsMap[activeMonth]}
+          </div>
           {/* <PaginationItem>
             <PaginationLink href="#">3</PaginationLink>
           </PaginationItem>
@@ -22,7 +56,7 @@ const MonthPager = () => {
             <PaginationEllipsis />
           </PaginationItem> */}
           <PaginationItem>
-            <PaginationNext href="#" />
+            <PaginationNext size="lg" className="select-none" onClick={switchToNextMonth} />
           </PaginationItem>
         </PaginationContent>
       </Pagination>

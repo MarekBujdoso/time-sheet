d7db16e5831091e5f36d40d2ccb2f5b9f4f4f807
MarekBujdoso <marek.bujdoso@gmail.com>
2025-03-26 21:17:10 +0100

    default times updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 // import { useState } from 'react'
-import Decimal from 'decimal.js'
-import './App.css'
-import ConfigContext from './app/sheet/ConfigContext'
-import Sheet from './app/sheet/Sheet'
+import Decimal from 'decimal.js';
+import './App.css';
+import ConfigContext from './app/sheet/ConfigContext';
+import Sheet from './app/sheet/Sheet';
 
 function App() {
   // const [count, setCount] = useState(0)
 
-// urobit tu context a provider pre nejaky config prihlaseneho usera
+  // urobit tu context a provider pre nejaky config prihlaseneho usera
   return (
-    <ConfigContext.Provider value={{ officialWorkTime: new Decimal(7.5), lunchBreak: 0.5, officialStartTime: { hours: 7, minutes: 30 }, officialEndTime: { hours: 15, minutes: 30 } }}>
+    <ConfigContext.Provider
+      value={{
+        officialWorkTime: new Decimal(7.5),
+        lunchBreak: 0.5,
+        officialStartTime: { hours: 7, minutes: 30 },
+        officialEndTime: { hours: 15, minutes: 30 },
+        defaultStartTime: { hours: 7, minutes: 30 },
+        defaultEndTime: { hours: 15, minutes: 30 },
+      }}
+    >
       <Sheet />
     </ConfigContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/sheet/ConfigContext.ts
+++ src/app/sheet/ConfigContext.ts
@@ -6,13 +6,17 @@ export interface ConfigContextType {
   lunchBreak: number
   officialStartTime: {hours: number, minutes: number}
   officialEndTime: {hours: number, minutes: number}
+  defaultStartTime: {hours: number, minutes: number}
+  defaultEndTime: {hours: number, minutes: number}
 }
 
 const ConfigContext = React.createContext<ConfigContextType>({
   officialWorkTime: new Decimal(7.5),
   lunchBreak: 0.5,
-  officialStartTime: {hours: 7, minutes: 30},
-  officialEndTime: {hours: 15, minutes: 30},
+  officialStartTime: {hours: 7, minutes: 30}, 
+  officialEndTime: {hours: 15, minutes: 30}, // calculated from startTime, officialWorkTime and lunchBreak
+  defaultStartTime: {hours: 7, minutes: 30},
+  defaultEndTime: {hours: 15, minutes: 30},
 })
 
 export default ConfigContext

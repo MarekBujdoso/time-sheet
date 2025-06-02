56ef69b5338449f7b93d906c0b763cf7180b6a2d
MarekBujdoso <marek.bujdoso@gmail.com>
2025-06-02 10:01:46 +0200

    remove default user name

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-// import { useState } from 'react'
 import Decimal from 'decimal.js';
 import './App.css';
 import ConfigContext from './app/sheet/ConfigContext';
 import Sheet from './app/sheet/Sheet';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // urobit tu context a provider pre nejaky config prihlaseneho usera
+  // config prihlaseneho usera
   return (
     <ConfigContext.Provider
       value={{
-        userName: 'Judita Bujdosoova',
+        userName: '',
         officialWorkTime: new Decimal(7.5),
         lunchBreak: 0.5,
         officialStartTime: { hours: 7, minutes: 30 },

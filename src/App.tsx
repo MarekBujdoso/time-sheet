f4d7c2eea377b3c4d6311245f159c8785ab01af7
MarekBujdoso <marek.bujdoso@gmail.com>
2025-10-01 19:43:07 +0200

    change config in provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import Decimal from 'decimal.js';
 import './App.css';
-import ConfigContext from './app/sheet/ConfigContext';
+import ConfigContext, { configDefaults } from './app/sheet/ConfigContext';
 import Sheet from './app/sheet/Sheet';
 
 function App() {
   // config prihlaseneho usera
   return (
-    <ConfigContext.Provider
-      value={{
-        userName: '',
-        officialWorkTime: new Decimal(7.5),
-        lunchBreak: 0.5,
-        officialStartTime: { hours: 7, minutes: 30 },
-        officialEndTime: { hours: 15, minutes: 0 },
-        defaultStartTime: { hours: 7, minutes: 30 },
-        defaultEndTime: { hours: 15, minutes: 0 },
-      }}
-    >
+    <ConfigContext.Provider value={configDefaults}>
       <Sheet />
     </ConfigContext.Provider>
   );
--- src/app/sheet/ConfigContext.ts
+++ src/app/sheet/ConfigContext.ts
@@ -11,14 +11,26 @@ export interface ConfigContextType {
   defaultEndTime: {hours: number, minutes: number}
 }
 
-const ConfigContext = React.createContext<ConfigContextType>({
-  userName: 'Janko Hrasko',
+export const configDefaults: ConfigContextType = {
+  userName: '',
   officialWorkTime: new Decimal(7.5),
   lunchBreak: 0.5,
   officialStartTime: {hours: 7, minutes: 30}, 
   officialEndTime: {hours: 15, minutes: 0}, // calculated from startTime, officialWorkTime and lunchBreak
   defaultStartTime: {hours: 7, minutes: 30}, // default - fixed time
-  defaultEndTime: {hours: 15, minutes: 0}, // default - fixed time
-})
+  defaultEndTime: {hours: 15, minutes: 0}, 
+}
+
+// export const configDefaults: ConfigContextType = {
+//   userName: '',
+//   officialWorkTime: new Decimal(6.25),
+//   lunchBreak: 0.5,
+//   officialStartTime: {hours: 7, minutes: 30}, 
+//   officialEndTime: {hours: 13, minutes: 45}, // calculated from startTime, officialWorkTime and lunchBreak
+//   defaultStartTime: {hours: 7, minutes: 30}, // default - fixed time
+//   defaultEndTime: {hours: 13, minutes: 45}, 
+// }
+
+const ConfigContext = React.createContext<ConfigContextType>(configDefaults)
 
 export default ConfigContext
